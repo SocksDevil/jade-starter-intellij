07ba4595298ea1288c7cbc076dda1a2de376b4fd
Andre Rocha <andrefmrocha@live.com.pt>
2020-10-01 15:21:58 +0100

    Add starter pack to use JADE in IntelliJ

---
+++ src/HelloWorldAgent.java
@@ -0,0 +1,9 @@
+import jade.core.Agent;
+
+// Example
+public class HelloWorldAgent extends Agent {
+    @Override
+    protected void setup() {
+        System.out.println("Hello World!");
+    }
+}
